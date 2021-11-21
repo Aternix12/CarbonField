d323e6f669643a793dedf82267653aff19746d26
Tristan Lee Mitchell <tristanmitchell2113@gmail.com>
2021-11-21 22:06:54 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CarbonField
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new CarbonField())
+                game.Run();
+        }
+    }
+}
