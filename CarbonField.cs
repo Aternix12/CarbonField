511e9fc0004f0b9121865795262ecfe103514a3e
Tristan Lee Mitchell <tristanmitchell2113@gmail.com>
2021-11-23 21:59:39 +1100

    Bouncing block

--- CarbonField.cs
+++ CarbonField.cs
@@ -9,6 +9,10 @@ namespace CarbonField
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D logo;
+        Vector2 _position = new Vector2(0, 0);
+        Vector2 _velocity = new Vector2(100, 100);
+
         public CarbonField()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -27,6 +31,8 @@ namespace CarbonField
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
+            logo = this.Content.Load<Texture2D>("spr_wallblock");
             // TODO: use this.Content to load your game content here
         }
 
@@ -36,15 +42,35 @@ namespace CarbonField
                 Exit();
 
             // TODO: Add your update logic here
+            _position += _velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            if(_position.X <= 0 && _velocity.X < 0)
+            {
+                _velocity.X *= -1; 
+            }
+            else if (_position.Y <= 0 && _velocity.Y < 0)
+            {
+                _velocity.Y *= -1;
+            }
+            else if (_position.X >= _graphics.GraphicsDevice.Viewport.Width - 32 && _velocity.X > 0)
+            {
+                _velocity.X *= -1;
+            }
+            else if (_position.Y >= _graphics.GraphicsDevice.Viewport.Height - 32 && _velocity.Y > 0)
+            {
+                _velocity.Y *= -1;
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();//These are the image layers
+            _spriteBatch.Draw(logo, _position, color: Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
