288002f7f8eb7aa1b0228ff37614bf881edec2e6
Tristan Lee Mitchell <tristanmitchell2113@gmail.com>
2022-03-11 16:09:45 +1100

    Added Bindings

---
+++ Bindings/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bindings
+{
+    internal class Constants
+    {
+        public const int MAX_PLAYERS = 100;
+
+    }
+}
